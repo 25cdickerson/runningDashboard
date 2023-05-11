0e42d1914feeb4fdea706f1a21cf8a9564c1d592
Christopher (Preston) Dickerson <cdickerson39@yahoo.com>
2023-05-10 22:54:53 -0400

    Fixed up the graph

--- dashboard.js
+++ dashboard.js
@@ -5,9 +5,7 @@ function openTab(evt, tabName, subTabName,  isInnerTab) {
 	// Get all elements with class="tabcontent" and hide them
 	tabcontent = document.getElementsByClassName("tabcontent");
 	for (i = 0; i < tabcontent.length; i++) {
-		if (!isInnerTab || tabcontent[i].id === tabName) {
-			tabcontent[i].style.display = "none";
-		}
+		tabcontent[i].style.display = "none";
 	}
 
 	// Get all elements with class="subtabcontent" and hide them
@@ -26,65 +24,4 @@ function openTab(evt, tabName, subTabName,  isInnerTab) {
 	document.getElementById(tabName).style.display = "block";
 	document.getElementById(subTabName).style.display = "block";
 	evt.currentTarget.className += " active";
-
-	// Create a chart for the currently viewed table
-	if (!isInnerTab && tabName === "high-school") {
-		createChart("high-school-chart", "High School Performances");
-	} else if (!isInnerTab && tabName === "college") {
-		createChart("college-chart", "College Performances");
-	}
 }
-
-  
-
-function createChart(chartId, chartTitle) {
-	var table = document.querySelector("#" + chartId).previousElementSibling;
-	var dates = Array.from(table.querySelectorAll("tbody tr td:first-child")).map((date) => date.textContent);
-	var times = Array.from(table.querySelectorAll("tbody tr td:last-child")).map((time) => time.textContent);
-
-	var data = {
-		labels: dates,
-		datasets: [
-			{
-				label: "Time (MM:SS)",
-				backgroundColor: "rgba(255, 99, 132, 0.2)",
-				borderColor: "rgba(255, 99, 132, 1)",
-				borderWidth: 1,
-				hoverBackgroundColor: "rgba(255, 99, 132, 0.4)",
-				hoverBorderColor: "rgba(255, 99, 132, 1)",
-				data: times,
-			},
-		],
-	};
-
-	var options = {
-		title: {
-			display: true,
-			text: chartTitle,
-			fontSize: 18,
-			fontStyle: "bold",
-		},
-		scales: {
-			yAxes: [
-				{
-					scaleLabel: {
-						display: true,
-						labelString: "Time (MM:SS)",
-						fontSize: 14,
-						fontStyle: "bold",
-					},
-				},
-			],
-		},
-	};
-
-	var ctx = document.getElementById(chartId).getContext("2d");
-	var myChart = new Chart(ctx, {
-		type: "line",
-		data: data,
-		options: options,
-	});
-}
-
-// Call the createChart function for the initially displayed tab
-createChart("high-school-chart", "High School Performances");
